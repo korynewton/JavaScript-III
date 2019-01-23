c3f6bd500839a5020efe36849ea0a326d007fbdc
korynewton <korynewtondev@gmail.com>
2019-01-23 12:30:22 -0800

    this.js complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,52 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding : unless otherwise spacified. Using "this" will refer to the global/window context of this. The global context is JavaScript in its entirety 
+* 2. Implicit binding: In this context "this" refers to the object in which it is referenced
+* 3. New binding: In this context "this" will refer to the new Object that was created, not the origin object
+* 4. Explicit Binding: Using the .call() or .apply() function it will be made known explicitly what object "this" will be in reference to
+*
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 : Window
 
-// code example for Window Binding
+console.log(this)
 
-// Principle 2
+// Principle 2: Implicit
 
-// code example for Implicit Binding
+const Person = {
+    firstname: 'Kory',
+    lastname: 'Newton',
+    introduction: function() {
+        console.log(`Hello my name is ${this.firstname} ${this.lastname}`)
+    }
+  };
+  Person.introduction();
 
-// Principle 3
+// Principle 3: New  
 
-// code example for New Binding
+function PerferedBeverage(beverage) {
+    this.first = 'My favorite drink in the morning is ';
+    this.beverage = beverage;
+    this.sentence = function() {
+      console.log(this.first + this.beverage);
+    };
+  }
+  
+  const coffee = new PerferedBeverage('coffee');
+  const tea = new PerferedBeverage('tea');
+  
+  coffee.sentence();
+  tea.sentence();
 
-// Principle 4
+// Principle 4: Explicit
 
-// code example for Explicit Binding
+const list = ['water', 'soda', 'milk', 'orange juice']
+
+function fullSentence(item1, item2, item3){
+    return `While I prefer ${this.beverage} in the morning, I also enjoy ${item1}, ${item2}, and ${item3}.`
+  }
+
+  console.log(fullSentence.apply(coffee, list))
